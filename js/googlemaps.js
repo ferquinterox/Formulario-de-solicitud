9f01febc6f8b163646639303298413883943794b
ferquinterox <36277653+ferquinterox@users.noreply.github.com>
2018-02-08 18:45:56 -0500

    Comentarios
    Se agregaron loss comentarios/documentación de los html, css (solicitud) y googlemaps.js

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,17 +1,20 @@
-   function initMap() {
-                        var panama = {lat: 8.981312, lng: -79.521658};
+/* Iniciador del mapa */  
+function initMap() {
+                        var panama = {lat: 8.981312, lng: -79.521658}; /*Posicion inicial, puse calle 50  */
                         var map = new google.maps.Map(document.getElementById('map'), {
-                          zoom: 15,
+                          zoom: 15, /* Zoom o que tan cerca estara el mapa de la posicion pedida*/
                           mapTypeId: google.maps.MapTypeId.ROADMAP,    
                           center: panama
                         });
                         var marker = new google.maps.Marker({
                           position: panama,
-                          draggable:true,
+                          draggable:true, /*Opcion que permite arrastrar el marcador */ 
                           map: map
                         });
 
    }
+
+/*Supuestamente funcion del mapa que permite elegir algo del mapa y ponerlo en los input  */
 google.maps.event.addListener(marker, 'dragend', function(evt) {   
     
         var latLng = new google.maps.LatLng(lat,lng);
