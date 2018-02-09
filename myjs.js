c9c6d2199d4f58d21ed2f5cc3fd135bed59e249c
manuelf1804 <36085610+manuelf1804@users.noreply.github.com>
2018-02-08 20:51:30 -0500

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,9 @@
+ $('#form').validator().on('submit', function (e) {
+  if (e.isDefaultPrevented()) { 
+      alert("El formulario no puede contener errores");
+  } else {
+    // se envia
+  }
+})
+
+
