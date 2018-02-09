dfbd4ae701a8b8d2b73804febdc1bf9b45f1ba2a
manuelf1804 <36085610+manuelf1804@users.noreply.github.com>
2018-02-08 20:55:40 -0500

    Update myjs.js
    se cambio #form por #formulario, ya que ese es el id del formulario.

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,10 @@
- $('#form').validator().on('submit', function (e) {
+ $('#formulario').validator().on('submit', function (e) {
   if (e.isDefaultPrevented()) { 
       alert("El formulario no puede contener errores");
   } else {
     // se envia
   }
 })
+
 
 
