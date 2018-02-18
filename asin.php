75799c061f91f83f904f7f37791c610ef9ca1b2f
manuelf1804 <36085610+manuelf1804@users.noreply.github.com>
2018-02-18 18:23:57 -0500

    Add files via upload
    Funcionan en conjunto prueba2 llama a asin

---
+++ asin.php
@@ -0,0 +1,13 @@
+<?php
+
+$co=$_POST['name'];
+ //if(isset($_POST['data'])){
+     //include("php/Conexion.php");
+     //$tipo=$_POST['data'];
+     //$obj=new Conexion();
+
+     echo $co." hola2";
+    
+
+ //}
+ ?>
